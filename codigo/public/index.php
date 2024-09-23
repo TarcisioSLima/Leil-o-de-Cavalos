02008a9caa1e063c45586a811020e3fee2c42ea6
samuelbatistadeb <samuelbatistadeb@gmail.com>
2024-09-23 10:13:27 -0300

    cavalos index marromeno

--- codigo/public/index.php
+++ codigo/public/index.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    include_once $_SERVER['DOCUMENT_ROOT'].'/db/conexao.php';
 ?>
 
 <!DOCTYPE html>
@@ -164,6 +165,12 @@
         .u_categorias li:hover{
             background-color: #dedddd;
         }
+        .disponivel{
+            background-color: rgb(0, 255, 0)            ;
+        }
+        .finalizado{
+            background-color:  #ff0000            ;
+        }
     </style>
 </head>
 <body>
@@ -307,10 +314,76 @@
     
     
     </div>
+    <?php
+    
+    $sql = "SELECT * FROM tb_lote";
+            $retorno = conectarDB("select", $sql, [], "");
+            foreach ($retorno[1] as $dados) { 
+                // print_r($dados);
+                $id_lote = $dados['id_lote'];
+                $titulo_lote = $dados['titulo_lote'];
+                $valor_lote = $dados['valor_lote'];
+                $data_lote = $dados['data_lote'];
+                $estado_lote = $dados['estado_lote'];
+                $tb_cavalo_id_cavalo = $dados['tb_cavalo_id_cavalo'];
 
-    <div class="lotes">
+                // Obter a data atual
+                $data_atual = new DateTime();
+                $data_evento = new DateTime($data_lote);
+
+                // Calcular a diferença
+                $diferenca = $data_atual->diff($data_evento);
+
+                // Obter dias, horas, minutos e segundos
+                $dias = $diferenca->d;
+                $horas = $diferenca->h;
+                $minutos = $diferenca->i;
+                $segundos = $diferenca->s;
+
+    ?>
+    
+            <div class="lotes">
+                <div class="ls">
+                    <img src="assets/img/horse.jpg" alt="" style="max-width: 100%; border-radius: 10px;  object-fit: cover;"> <br>
+                    <hr> <br>
+                    <h4>
+                    <?= $titulo_lote?>
+                    </h4> <br>
+                    <hr><br>
+                    <p><?= $valor_lote?></p>
+                    <br>
+                    <hr>
+                    <p <?php if ($estado_lote == 'Disponível') {
+                        echo "class='disponivel'";} 
+                        else {echo "class='finalizado'";}?> ><?= $estado_lote?></p>
+                    <br>
+                    <hr>
+                <div class="uls">
+                        <ul class="ul_dias">
+                            <li class="nuns"><?= $dias?></li>
+                            <li>Dias</li>
+                        </ul>
+                        <ul class="ul_horas">
+                            <li class="nuns"><?= $horas?></li>
+                            <li>Horas</li>
+                        </ul>
+                        <ul class="ul_minutos">       
+                            <li class="nuns"><?= $minutos?></li>
+                            <li>Minutos</li>
+                        </ul>
+                        <ul class="ul_segundos">
+                            <li class="nuns"><?= $segundos?></li>
+                            <li>Segundos</li>
+                        </ul>
+                    
+                </div>
+            </div>
+
+          <?php } ?>  
+
+    <!-- <div class="lotes">
         <div class="ls">
-            <img src="assets/img/horse.jpg" alt="" style="max-width: 100%; border-radius: 10px;  object-fit: cover;"> <br>
+             <img src="assets/img/horse.jpg" alt="" style="max-width: 100%; border-radius: 10px;  object-fit: cover;"> <br>
             <hr> <br>
             <h4>
                 Item de Exemplo 1
@@ -337,8 +410,8 @@
                     <li>Segundos</li>
                 </ul>
             
-        </div>
-        
+        </div> -->
+<!--         
         </div>
         <div class="ls">
             <img src="assets/img/horse.jpg" alt="" style="overflow: hidden;"> <br>
@@ -430,7 +503,7 @@
                 
         </div>
             
-    </div>
+    </div> --> 
 <br><br><br><br><br><br>
 
     <script>
