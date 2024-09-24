3361e9ccaa2488b470f478eb964a933ae34ddbe0
samuelbatistadeb <samuelbatistadeb@gmail.com>
2024-09-24 08:52:17 -0300

    banco e começo do lote

--- codigo/public/index.php
+++ codigo/public/index.php
@@ -314,53 +314,35 @@
     
     </div>
     <?php
-       $sql = "SELECT * FROM tb_cavalo";
+       $sql = "SELECT * FROM tb_cavalo WHERE destaque = 'Sim'";
        $retorno = conectarDB("select", $sql, [], "");
        foreach ($retorno[1] as $dados) { 
-           $id_lote = $dados['id_lote'];
-           $titulo_lote = $dados['titulo_lote'];
-           $valor_lote = $dados['valor_lote'];
-           $estado_lote = $dados['estado_lote'];
-           $tb_cavalo_id_cavalo = $dados['tb_cavalo_id_cavalo'];
-
-           // Obter a data atual
-
+           $id_cavalo = $dados['id_cavalo'];
+           $nome_cavalo = $dados['nome_cavalo'];
+           $raca_cavalo = $dados['raca_cavalo'];
+           $pelagem_cavalo = $dados['pelagem_cavalo'];
+           $premio_cavalo = $dados['premio_cavalo'];
+           $situacao_cavalo = $dados['situacao_cavalo'];
+           $modalidade_cavalo = $dados['modalidade_cavalo'];
+           $destaque = $dados['destaque'];
+           $img_cavalo = $dados['img_cavalo'];
 ?>
             <div class="lotes">
                 <div class="ls">
                     <img src="assets/img/horse.jpg" alt="" style="max-width: 100%; border-radius: 10px;  object-fit: cover;"> <br>
                     <hr> <br>
-                    <h4>
-                    <?= $titulo_lote?>
+                    <h4> Nome: 
+                    <?= $nome_cavalo?>
                     </h4> <br>
                     <hr><br>
-                    <p><?= $valor_lote?></p>
+                    <p>Raça: <?= $raca_cavalo?></p>
                     <br>
                     <hr>
-                    <p <?php if ($estado_lote == 'Disponível') {
-                        echo "class='disponivel'";} 
-                        else {echo "class='finalizado'";}?> ><?= $estado_lote?></p>
+                    <p> Prêmios: <?= $premio_cavalo?></p>
                     <br>
                     <hr>
-                <div class="uls">
-                        <ul class="ul_dias">
-                            <li class="nuns"><?= $dias?></li>
-                            <li>Dias</li>
-                        </ul>
-                        <ul class="ul_horas">
-                            <li class="nuns"><?= $horas?></li>
-                            <li>Horas</li>
-                        </ul>
-                        <ul class="ul_minutos">       
-                            <li class="nuns"><?= $minutos?></li>
-                            <li>Minutos</li>
-                        </ul>
-                        <ul class="ul_segundos">
-                            <li class="nuns"><?= $segundos?></li>
-                            <li>Segundos</li>
-                        </ul>
-                    
-                </div>
+                    <p>Modalidade: <?= $modalidade_cavalo?></p>                    
+                    <br>
             </div>
 
           <?php } ?>  
