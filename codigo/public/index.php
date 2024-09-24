399e81f42a1d156b4b1f67c0bffbf4ff96a903b3
João Marcelo <contatoawster@gmail.com>
2024-09-24 09:43:33 -0300

    n sei

--- codigo/public/index.php
+++ codigo/public/index.php
@@ -294,7 +294,7 @@
                        <?php } ?>
                     </ul>
                     <ul>
-                        <img src="<?php $img_cavalo ?>" alt="imagem" class="img">
+                        <img src="<?= $img_cavalo ?>" alt="imagem" class="img">
                     </ul>
                 </div>
 
