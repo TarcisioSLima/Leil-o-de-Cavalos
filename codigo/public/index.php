9eaf38da96fb89c70de8a39f94aa08dfc1bba1dd
João Marcelo <contatoawster@gmail.com>
2024-10-08 08:58:31 -0300

    mexendo na pesquisa 1

--- codigo/public/index.php
+++ codigo/public/index.php
@@ -165,8 +165,9 @@
         <div class="logo" >
            <img src="assets/img/logo_verde.png" alt="" style="max-width: 200px; max-height: 200px;">
         </div>
-        <form action="index.php?pesquisa=W80pl">
+        <form action="index.php">
         <div class="search-container">
+            <!-- <input type="hidden" name="pesquisa" value = "qualquercoisa"> -->
                 <select name="filtro" id="" class="search-box">
                     <option value="raca_cavalo">Raça</option>
                     <option value="pelagem_cavalo">Pelagem</option>
@@ -238,7 +239,7 @@
         </ul>
     </div>
     <?php 
-        if (!isset($_REQUEST["pesquisa"])) { ?>
+        if (!isset($_REQUEST["texto"])) { ?>
             <div class="container">
                 <!-- Menu lateral de cavalos em destaque -->
                 
@@ -340,10 +341,40 @@
         $texto = $_REQUEST["texto"];
         switch ($filtro) {
             case 'raca_cavalo':
-                $sql = "SELECT * FROM tb_cavalo WHERE raca_cavalo like %?%";
-                $retorno = conectarDB("select", $sql, [$texto], "s");
+                $sql = "SELECT * FROM tb_cavalo WHERE raca_cavalo LIKE ? AND situacao_cavalo = 'Ativo'"; 
+                $param = "%" . $texto . "%"; // Adiciona os % ao redor do texto
+                $retorno = conectarDB("select", $sql, [$param], "s");
                 if (sizeof($retorno[1]) > 0) {
-                   
+                    foreach ($retorno[1] as $dados) { 
+                        // Dados do cavalo
+                        $id_cavalo = $dados["id_cavalo"];
+                        $nome_cavalo = $dados["nome_cavalo"];
+                        $raca_cavalo = $dados["raca_cavalo"];
+                        $pelagem_cavalo = $dados["pelagem_cavalo"];
+                        $premio_cavalo = $dados["premio_cavalo"];
+                        $modalidade_cavalo = $dados["modalidade_cavalo"];
+                        $img_cavalo = $dados["img_cavalo"];
+
+
+                        // $data_fechamento_conversao = new DateTime($data_fechamento);
+                        // $data_final = $data_fechamento_conversao ->format('d/m/Y');
+                ?>
+                        <div class="card">
+                            <img src="<?= $img_cavalo?>" alt="Imagem do cavalo <?= $nome_cavalo ?>" class="card-img">
+                            <div class="card-content">
+                                <h3 class="card-title"><?= $nome_cavalo ?></h3>
+                                <p class="card-text"><strong>Raça:</strong> <?= $raca_cavalo ?></p>
+                                <p class="card-text"><strong>Pelagem:</strong> <?= $pelagem_cavalo ?></p>
+                                <p class="card-text"><strong>Prêmios:</strong> <?= $premio_cavalo ?></p>
+                                <p class="card-text"><strong>Modalidade:</strong> <?= $modalidade_cavalo ?></p>
+
+                            <div class="card-actions">
+                                <a href="#" class="card-link">Dar lance</a>
+                            </div>
+                            </div>
+                        </div>
+                <?php  
+                    }
                 }
                 break;
             case 'pelagem_cavalo':
@@ -354,139 +385,16 @@
                 $sql = "SELECT * FROM tb_cavalo WHERE premio_cavalo like %?%";
                 conectarDB("select", $sql, [$texto], "s");
                 break;
-            
+
             default:
                 
                 break;
+            }
         }
         
 
+
     ?>
-        
-<?php } ?>
-<!-- <div class="lotes">
-    <div class="ls">
-        <img src="assets/img/horse.jpg" alt="" style="max-width: 100%; border-radius: 10px;  object-fit: cover;"> <br>
-        <hr> <br>
-        <h4>
-            Item de Exemplo 1
-        </h4> <br>
-        <hr><br>
-        <p>R$ 12.000,00</p>
-        <br>
-        <hr>
-        <div class="uls">
-            <ul class="ul_dias">
-                <li class="nuns">4</li>
-                <li>Dias</li>
-            </ul>
-                <ul class="ul_horas">
-                    <li class="nuns">7</li>
-                    <li>Horas</li>
-                </ul>
-                <ul class="ul_minutos">       
-                    <li class="nuns">40</li>
-                    <li>Minutos</li>
-                </ul>
-                <ul class="ul_segundos">
-                    <li class="nuns">10</li>
-                    <li>Segundos</li>
-                </ul>
-            
-        </div> -->
-<!--         
-        </div>
-        <div class="ls">
-            <img src="assets/img/horse.jpg" alt="" style="overflow: hidden;"> <br>
-            <hr> <br>
-            <h4>
-                Item de Exemplo 2
-            </h4>
-            <br><hr>
-            <br>
-            <p>R$ 4.700,00</p>
-            <br><hr>
-        <div class="uls">
-
-            <ul class="ul_dias">
-                <li class="nuns">4</li>
-                <li>Dias</li>
-            </ul>
-            <ul class="ul_horas">
-                <li class="nuns">7</li>
-                <li>Horas</li>
-            </ul>
-            <ul class="ul_minutos">       
-                <li class="nuns">40</li>
-                <li>Minutos</li>
-            </ul>
-            <ul class="ul_segundos">
-                <li class="nuns">10</li>
-                <li>Segundos</li>
-            </ul>
-        </div>
-        
-
-        </div>
-        <div class="ls">
-            <img src="assets/img/horse.jpg" alt=""> <br>
-            <hr> <br>
-            <h4>
-                Item de Exemplo 3
-            </h4>
-            <br> <hr><br>
-            <p>R$ 9.000,00</p>
-            <br><hr>
-            <div class="uls">
-                <ul class="ul_dias">
-                    <li class="nuns">4</li>
-                    <li>Dias</li>
-                </ul>
-                <ul class="ul_horas">
-                    <li class="nuns">7</li>
-                    <li>Horas</li>
-                </ul>
-                <ul class="ul_minutos">       
-                    <li class="nuns">40</li>
-                    <li>Minutos</li>
-                </ul>
-                <ul class="ul_segundos">
-                    <li class="nuns">10</li>
-                    <li>Segundos</li>
-                </ul>
-            </div>
-               
-        </div>
-        <div class="ls">
-                <img src="assets/img/horse.jpg" alt=""> <br> <hr> <br>
-                <h4>
-                    Item de Exemplo 4
-                </h4>
-                <br> <hr><br>
-                <p>R$ 4.000,00</p>
-                <br><hr>
-                <div class="uls">
-                    <ul class="ul_dias">
-                        <li class="nuns">4</li>
-                        <li>Dias</li>
-                    </ul>
-                    <ul class="ul_horas">
-                        <li class="nuns">7</li>
-                        <li>Horas</li>
-                    </ul>
-                    <ul class="ul_minutos">       
-                        <li class="nuns">40</li>
-                        <li>Minutos</li>
-                    </ul>
-                    <ul class="ul_segundos">
-                        <li class="nuns">10</li>
-                        <li>Segundos</li>
-                    </ul>
-                </div>
-                
-        </div>
-            
-    </div> --> 
 <br><br><br><br><br><br>
 
     <script>
