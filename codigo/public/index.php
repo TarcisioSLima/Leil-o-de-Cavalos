767303082be69b12dd80ceca936df58bc01cbcac
João Marcelo <contatoawster@gmail.com>
2024-11-28 09:39:23 -0300

    a

--- codigo/public/index.php
+++ codigo/public/index.php
@@ -763,12 +763,7 @@
     }
   ?>
 
-<<<<<<< HEAD
-<br><br><br><br><br><br>
-<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-=======
 <br><br><br><br><br><br> -->
->>>>>>> 8b7743f7c0d9d8730257bf01493cb9204a93cd3e
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script>
